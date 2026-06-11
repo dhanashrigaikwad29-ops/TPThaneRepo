1b0cdfda0e7676af4c9f887aa27e7b751126912c
dhanashrigaikwad29-ops <dhanashrigaikwad29@gmail.com>
2026-06-11 15:20:18 +0530

    ghb

--- TP_ThanePractice/src/main/java/SamplePack/SampleClass.java
+++ TP_ThanePractice/src/main/java/SamplePack/SampleClass.java
@@ -3,10 +3,6 @@ package SamplePack;
 public class SampleClass {
 	public static void main(String[] args) {
 		System.out.println("Dhanahsri");
-<<<<<<< HEAD
-		System.out.println("==============");
-=======
-		System.out.println("LKjhgfdsdfghjkl/");
->>>>>>> refs/heads/master
+		System.out.println("======asdfnm========");
 	}
 }
